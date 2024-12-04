8f4ed56efe1300823f57d40a0fb5caf8c6e1c626
Rhythm-KC <rhythmkc7@gmail.com>
2024-12-03 19:12:26 -0500

    Added new __init__.py to so utils is an module. Changes set_env_variable to get_env_variable

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,6 +1,6 @@
 import os
-def set_env_variables(variable_name:str, local_var: any):
+def get_env_variables(variable_name:str):
     var_val =os.getenv(variable_name) 
     if not var_val :
         raise KeyError(f"Environemnt variable {variable_name}  not found. Run the setup script to configure the project")
-    local_var = var_val
+    return var_val
