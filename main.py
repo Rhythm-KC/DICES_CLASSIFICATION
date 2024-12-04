b54c65e1731b07caea139695d9265b9e845e3834
Rhythm-KC <rhythmkc7@gmail.com>
2024-12-03 19:11:10 -0500

    ADDED correct imports and better traceback whern error

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-import src.CreateDataset as createdataset
-import src.run_classification as classify
+import sys
+import os
+import traceback
+from dotenv import load_dotenv
+
+sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
+load_dotenv()
+
+import CreateDataset as createdataset
+import run_classification as classify
 
 try:
     createdataset.run()
     classify.main()
 
 except Exception as e:
-    print(f"ran into exceptions \n{e.with_traceback}")
+    print(f"ran into exceptions \n{traceback.format_exc()}")
     
